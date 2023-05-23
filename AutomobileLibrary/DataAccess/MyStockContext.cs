2331a52a43881aae77709dfb2199133c9c4b3352
PC <PC@DESKTOP-0TNJS77>
2023-05-24 01:50:09 +0700

    Add project files.

---
+++ AutomobileLibrary/DataAccess/MyStockContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomobileLibrary.DataAccess;
+
+public partial class MyStockContext : DbContext
+{
+    public MyStockContext()
+    {
+    }
+
+    public MyStockContext(DbContextOptions<MyStockContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Car> Cars { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("server =DESKTOP-0TNJS77; database = MyStock;uid=sa;pwd=123;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Car>(entity =>
+        {
+            entity.HasKey(e => e.CarId).HasName("PK__Cars__68A0342E392FDEBF");
+
+            entity.Property(e => e.CarId).ValueGeneratedNever();
+            entity.Property(e => e.CarName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Manufacturer)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("money");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AutomobileWPFApp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AutomobileLibrary.Respository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutomobileWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton(typeof(ICarRespository), typeof(CarRespository));
+            services.AddSingleton<WindowCarManagement>();
+        }
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var WindowCarManagement = serviceProvider.GetService<WindowCarManagement>();
+            WindowCarManagement.Show();
+        }
+    }
+}
